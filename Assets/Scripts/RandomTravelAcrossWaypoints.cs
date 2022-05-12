d63e18cb20f7e213190a705a680ff88823b0fe4f
Asylbek Karashev <asylbekka@asisnur.kg>
2022-05-12 17:41:01 +0600

    designing seems complete, added one agent with random travel

---
+++ Assets/Scripts/RandomTravelAcrossWaypoints.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class RandomTravelAcrossWaypoints : MonoBehaviour
+{
+    [SerializeField] private WaypointsBuilder waypointsBuilder;
+    private int waypointsRange;
+    private List<Vector3> _waypoints;
+    private NavMeshAgent navMeshAgent;
+    private void Start()
+    {
+        Invoke(nameof(SetCourse), 2f);
+    }
+
+    private void SetCourse()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        _waypoints = WaypointsBuilder.Waypoints;
+        if(_waypoints == null)
+        {
+            Debug.LogError("NOT FOUND");
+        }
+        else
+        {
+            waypointsRange = _waypoints.Count;
+            Vector3 destination = new Vector3();
+            destination = _waypoints[UnityEngine.Random.Range(0, waypointsRange)];
+            //Debug.Log(destination);
+            navMeshAgent.SetDestination(destination);    
+        }
+    }
+}
---
+++ Assets/Scripts/WaypointsBuilder.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WaypointsBuilder : MonoBehaviour
+{
+    public static List<Vector3> Waypoints
+    {
+        get { return waypoints; }
+    }
+    private static List<Vector3> waypoints;
+    [SerializeField] private Vector3 stepping = Vector3.one;
+    private void Awake()
+    {
+        waypoints = new List<Vector3>();
+        // grab necessary components
+        NavMeshAgent navMeshAgent = GetComponent<NavMeshAgent>();
+        BoxCollider boxCollider = GetComponent<BoxCollider>();
+        // setting dimensions for loop search
+        float startX = transform.position.x + boxCollider.center.x - boxCollider.size.x / 2f;
+        float endX = transform.position.x + boxCollider.center.x + boxCollider.size.x / 2f;
+        float startY = transform.position.y + boxCollider.center.y - boxCollider.size.y / 2f;
+        float endY = transform.position.y + boxCollider.center.y + boxCollider.size.y / 2f;
+        float startZ = transform.position.z + boxCollider.center.z - boxCollider.size.z / 2f;
+        float endZ = transform.position.z + boxCollider.center.z + boxCollider.size.z / 2f;
+        //Debug.Log("START: " + startX + " : " + startY + " : " + startZ);
+        //Debug.Log("END: " + endX + " : " + endY + " : " + endZ);
+        // loop search for valid path for waypoints
+        NavMeshPath navMeshPath = new NavMeshPath();
+        for (float x = startX; x <= endX; x += stepping.x)
+        {
+            for (float z = startZ; z <= endZ; z += stepping.z)
+            {
+                for (float y = startY; y <= endY; y += stepping.y)
+                {
+                    Vector3 target = new Vector3(x, y, z);
+                    //Debug.Log(x + "_" + y + "_" + z);
+                    // check if the location is valid
+                    if (navMeshAgent.CalculatePath(target, navMeshPath) && navMeshPath.status == NavMeshPathStatus.PathComplete)
+                    {
+                        //Debug.Log(target);
+                        waypoints.Add(target);
+                        //Debug.Log("ADDED");
+                    }
+                }
+            }
+        }
+    }
+
+}
